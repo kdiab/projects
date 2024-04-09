e03b829dd6b03f2686213d9a9a57a2f73d7412c4
kdiab <khalid@diab.cc>
2024-04-09 16:01:59 -0400

    calculating terminal height with fallback for unsupported terminals

--- miv/miv.c
+++ miv/miv.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <sys/ioctl.h>
 
 /*** defines ***/
 void handleExit();
@@ -14,6 +15,8 @@ void handleExit();
 /*** global state ***/
 
 struct editorConfig {
+	int screenrows;
+	int screencols;
 	struct termios term;
 };
 
@@ -54,6 +57,35 @@ char readKey(){
 	return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  	char buf[32];
+  	unsigned int i = 0;
+  	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+  	while (i < sizeof(buf) - 1) {
+  	  	if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+  	  	if (buf[i] == 'R') break;
+  	  	i++;
+  	}
+  	buf[i] = '\0';
+
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+	return 0;
+}
+
+int getWindowSize(int *rows, int *cols){
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		return getCursorPosition(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
 /*** input ***/
 
 void processKeys(){
@@ -75,7 +107,7 @@ void handleExit(){
 
 void drawRows(){
 	int y;
-	for (y = 0; y < 24; y++) {
+	for (y = 0; y < E.screenrows; y++) {
 		write (STDOUT_FILENO, "~\r\n", 3);
 	}
 }
@@ -90,8 +122,14 @@ void refreshScreen(){
 
 /*** init and main loop ***/
 
+void init() {
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
 	enableRawMode();
+	init();
+
 	while (1){
 		refreshScreen();
 		processKeys();
