fc99ac9829d61e2b0785b1050aa018ac80064d3c
kdiab <khalid@diab.cc>
2024-04-11 17:28:49 -0400

    Add support for PAGE_UP and PAGE_DOWN keys, though not handling both esc sequence cases

--- miv/miv.c
+++ miv/miv.c
@@ -18,7 +18,9 @@ enum keymap {
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
-	ARROW_DOWN
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 /*** global state ***/
@@ -68,13 +70,22 @@ int readKey() {
 		char seq[3];
 		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
 		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+		
 
 		if (seq[0] == '['){
+			if (seq[2] >= '0' && seq[2] <= '9'){
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+					switch(seq[1]) {
+				}
+			} else {
 			switch (seq[1]) {
 				case 'A': return ARROW_UP;
 				case 'B': return ARROW_DOWN;
 				case 'C': return ARROW_RIGHT;
 				case 'D': return ARROW_LEFT;
+				case '5': return PAGE_UP;
+				case '6': return PAGE_DOWN;
+				}
 			}
 		}
 		return '\x1b';
@@ -175,6 +186,15 @@ void processKeys() {
 		case ARROW_LEFT:
 			moveCursor(c);
 			break;
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				int times = E.screenrows;
+				while (times --)
+					moveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
+			break;
+
 	}
 }
 
