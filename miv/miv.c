2f1e324ecd9be63bebb0dbda8bbfdd8d249399d5
kdiab <khalid@diab.cc>
2024-05-06 20:30:13 -0400

    search forward and backwards

--- miv/miv.c
+++ miv/miv.c
@@ -373,15 +373,37 @@ void save() {
 /*** find / search ***/
 
 void findCallback(char *query, int key) {
+	static int last_match = -1;
+	static int direction = 1;
+
 	if (key == '\r' || key == '\x1b') {
+		last_match = -1;
+		direction = 1;
 		return;
+	} else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+		direction = 1;
+	} else if (key == ARROW_LEFT || key == ARROW_UP) {
+		direction = -1;
+	} else {
+		last_match = -1;
+		direction = 1;
 	}
+	
+	if (last_match == -1) direction = 1;
+	int current = last_match;
+
 	int i;
 	for (i = 0; i < E.numrows; i++) {
-		trow *row = &E.row[i];
+		current += direction;
+		if (current == -1) current = E.numrows - 1;
+		else if (current == E.numrows) current = 0;
+
+
+		trow *row = &E.row[current];
 		char *match = strstr(row->render, query);
 		if (match) {
-			E.cy = i;
+			last_match = current;
+			E.cy = current;
 			E.cx = rxtocx(row, match - row->render);
 			E.rowoffset = E.numrows;
 			break;
@@ -719,7 +741,7 @@ int main(int argc, char *argv[]) {
 		Open(argv[1]);
 	}
 
-	setStatusMessage("HELP: Ctrl-Q to quit | Ctrl-S to Save | Ctrl-F = find");
+	setStatusMessage("HELP: Ctrl-Q to quit | Ctrl-S to Save | Ctrl-F = find | ARROW KEYS TO CYCLE MATCHES");
 
 	while (1) {
 		refreshScreen();
